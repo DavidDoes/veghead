d70eb97260ea265ec69777fc1016822b9631e7b4
David Clark <dlc.clark@gmail.com>
2018-06-11 19:52:48 -0700

    update api calls

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 
 const PLACES_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
 const DETAILS_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/details/json?';
+const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address';
 
 function buildPlaces(userLocale, callback){
     console.log('buildPlaces called')
@@ -19,7 +20,7 @@ function buildPlaces(userLocale, callback){
 function buildDetails() {
     console.log('buildDetails called')
     const params = {
-        key: 'a11a69edeac01748b3eec35cf46febba',
+        key: 'AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0',
         placeid: `${placeID}`
     }
     $.getJSON(DETAILS_SEARCH_URL, params, callback)
@@ -28,6 +29,23 @@ function buildDetails() {
 function getData(data){
     console.log(data);
     const results = data.items.map((item) => generateResults(item));
+}
+
+// var lat = '';
+// var lng = '';
+// var address = `${userLocale}`;
+// geocoder.geocode( { 'address': address}, function(results, status) {
+//   if (status == google.maps.GeocoderStatus.OK) {
+//      lat = results[0].geometry.location.lat();
+//      lng = results[0].geometry.location.lng();
+//     });
+//   } else {
+//     alert("Geocode was not successful for the following reason: " + status);
+//   }
+// });
+
+function geocodePostal(userLocale){
+    
 }
  
 // navigator.geolocation.getCurrentPosition((pos)=>console.log(pos))
@@ -38,6 +56,7 @@ function listenSubmit(){
         const userLocaleTarget = $(event.currentTarget).find('.js-userLocale');
         const userLocale = userLocaleTarget.val();
         userLocaleTarget.val("");
+        geocodePostal(userLocale);
         buildPlaces(userLocale, getData);        
     });
 };
