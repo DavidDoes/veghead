8e805b37571212ec9c83d40bbbd97288765c9c88
David Clark <dlc.clark@gmail.com>
2018-06-19 21:29:44 -0700

    innerHTML error

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,80 @@
-'use strict';
-//KNOWN ISSUES
-// - Callback in GeoCoder?
-// - How do we get Geocoded info to next function? 
-// - How do we avoid callback hell?`
-// - How do we access API object? 
+// 'use strict';
 
-//Places - Nearby Search
-// const PLACES_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?input=vegan&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&key=AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0'; 
-//Places - Find by Text
-const PLACES_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=vegan&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&key=AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0'; 
-//need to pinpoint user's location in this URL somehow
-const DETAILS_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/details/json?';
-// const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+//Global API variables
+var geocoder;
+var map;
+var service;
+var markers = Array();
+var infos = Array();
 
-const GEOCODE_URL = `https://maps.googleapis.com/maps/api/geocode/json?postal_code=`;
-const key = 'AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0';
-
-function geoCoder(loc, getPlaces){
-    //take user's inputted postal at `${loc}` and convert to ltlng
-    console.log('geoCoder called');
-    // let URL = `${GEOCODE_URL}${loc}&${key}`;
-    const settings = {
-        datatype: 'jsonp'
-    }
-    $.ajax(URL, settings, getPlaces);
-}
-    // below doesn't feel right - this might be best used in getPlaces func?
-    // const settings = {
-    //     url: GEOCODE_URL,
-    //     data: {
-    //             key: 'AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0',
-    //             address: `${loc}`, //plugged in from listenSubmit    
-    //         },
-    //         dataType: 'json',
-    //         crossDomain: true,
-    //         type: 'GET',
-    //         success: callback
-    //     };
-    //     $.ajax(settings);
-    //     console.log('Directly from user input: ' + `${loc}`);
-    //     console.log('From settings.data.address: ' + settings.data.address);
-    //above two should be the same
-    // }
-
-
-function getPlaces(data, callback){
+//get nearby restaurants 
+function getPlaces(loc){
     console.log('getPlaces called');
-    // store data from API object
-    // const userLocation = 
-    // const settings = {
-    //     url: PLACES_SEARCH_URL,
-    //     data: {
+    var address = `${loc}`
+    console.log(address);
+    //geocode user postal
+    geocoder.geocode({'address':address}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK){ //if everything checks out
+            var addrLocation = results[0].geometry.location;
+            // map.setCenter(addrLocation);
+            //store coords in hidden variables:
+            document.getElementById('lat').value = results[0].geometry.location.lat();
+            document.getElementById('lng').value = results[0].geometry.location.lng();
 
-    //     }
-    // }
-    // $.getJSON(PLACES_SEARCH_URL, params).then(callback)
-    // .catch(err => console.log(err));
-    // getDetails(loc);
-}
+            //add new marker
+            var addrMarker = new google.maps.Marker({
+                position: addrLocation, //from above
+                map: map,
+                title: results[0].formatted_address, //from Google API object
+                icon: 'favicon.png'
+            });
+            var type = "restaurant";
+            var radius = "1000";
+            var lat = document.getElementById('lat').value;
+            var lng = document.getElementById('lng').value;
+            var cur_location = new google.maps.LatLng(lat, lng);
+            //request to Places
+            var request = {
+                location: cur_location,
+                radius: radius,
+                types: [type],
+                keyword: 'vegan'
+            };
+            service = new google.maps.places.PlacesService(map); 
+            service.nearbySearch(request, createMarkers); //use nearbySearch service
+            service.getDetails(request, createMarkers); //user getDetails service
+        } else {
+            alert('Please enter a valid postal code.')
+        }
+    });
+};
 
-function showResults(){
+function createMarkers(results, status){
+    console.log('createMarkers called');
     //generate HTML display Places and Details
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        createMarker(results[i]); //built-in API function
+        }
+    }
 }
 
-//not implemented
-function getDetails(placeId, callback){
-    console.log('getDetails called');
-
-    const details = {
-        key: 'AIzaSyCT4F67piVv6cvASPssAR1s_buPw6kBQw0',
-        placeid: `${placeId}`
-    }
-    // $.getJSON(DETAILS_SEARCH_URL, params, callback)
+function createMarker(obj){
+    var image = 'favicon.png';
+    var mark = new google.maps.Marker({
+        position: obj.geometry.location,
+        map: map,
+        title: obj.name,
+        icon: image
+    });
+    markers.push(mark); //send to marks global var, which is an array
+    //display info at marker:
+    var infowindow = new google.maps.Infowindow({
+        content: 
+        'obj.name + obj.vicinity'
+    });
+    infos.push(infowindow); //send to infos global var, which is an array
 }
 
 function listenSubmit(){
@@ -79,12 +82,22 @@ function listenSubmit(){
         event.preventDefault();
         console.log('submit button clicked');
         // const locationGetter = navigator.geolocation.getCurrentPosition((pos)=> getPlaces(pos));
-        const locationGetter = $(event.currentTarget).find('.js-userLocale');
+        const locationGetter = $(event.currentTarget).find('#js-userPostal');
         const location = locationGetter.val();
-        geoCoder(location, getPlaces); //push location to geoCoder, run getPlaces after
+        getPlaces(location); //push location to geoCoder, run getPlaces after
     })
 }
 
+function initMap(){
+    geocoder = new google.maps.Geocoder();
+    var myOptions = { //custom map styling
+        zoom: 10
+    };
+    //generate new map at 'gmap_results' id in HTML
+    var map = new google.maps.Map(document.getElementById('gmap_results'), myOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
 $(listenSubmit);
 
 
